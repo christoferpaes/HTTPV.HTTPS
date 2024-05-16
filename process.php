759f33acdab4618624a023ea757269046fa7423a
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2024-05-16 13:02:54 -0700

    Create process.php

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+// Access the submitted data using $_POST
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+// Process the data (e.g., display a response)
+echo "Hello, " . $name . "! Your email address is " . $email;
+?>
